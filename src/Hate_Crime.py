3d03267a7589d90208cde4a46a0552a7f04c58d7
Christopher Vicks <christopher.vicks@gmail.com>
2023-08-07 11:50:33 -0400

    modified all databases

---
+++ src/Hate_Crime.py
@@ -0,0 +1,91 @@
+import sys
+import os
+
+import pandas as pd
+
+class HateCrimeAnalyzer:
+    def __init__(self, data_path):
+        self.data = pd.read_csv(data_path)
+
+    def sum_offenses_by_year(self):
+        """Summarizes the number of offenses by year.
+
+        Returns:
+            DataFrame containing the summed data.
+        """
+        summed_data = self.data.groupby(['DATA_YEAR', 'OFFENSE_NAME'])['INCIDENT_ID'].count().reset_index()
+        summed_data.rename(columns={'INCIDENT_ID': 'Number of Offenses'}, inplace=True)
+        return summed_data
+
+    def calculate_and_print_victim_counts(self):
+        victim_counts = self.data['VICTIM_COUNT'].value_counts()
+        print(victim_counts)
+
+    def map_to_severity(self, offense_type):
+            violent_lst = ["Simple Assault", "Intimidation", "Aggravated Assault", "Robbery",\
+                          "Burglary/Breaking & Entering","Destruction/Damage/Vandalism of Property;Intimidation",\
+                          "Burglary/Breaking & Entering;Destruction/Damage/Vandalism of Property",\
+                            "Aggravated Assault;Simple Assault", "Arson","Destruction/Damage/Vandalism of Property;Simple Assault",\
+                            "Intimidation;Simple Assault","Murder and Nonnegligent Manslaughter",\
+                            "Aggravated Assault;Destruction/Damage/Vandalism of Property","Rape",\
+                            "Arson;Destruction/Damage/Vandalism of Property","Destruction/Damage/Vandalism of Property;Not Specified",\
+                            "Arson;Intimidation","Aggravated Assault;Intimidation","Weapon Law Violations",\
+                            "Aggravated Assault;Murder and Nonnegligent Manslaughter","Kidnapping/Abduction;Robbery","Fondling",\
+                            "Pornography/Obscene Material","Robbery;Simple Assault","Sexual Assault With An Object","Kidnapping/Abduction",\
+                            "Arson;Burglary/Breaking & Entering","Sodomy","Rape;Robbery","Destruction/Damage/Vandalism of Property;Intimidation;Not Specified",\
+                            "Arson;Burglary/Breaking & Entering;Destruction/Damage/Vandalism of Property",\
+                            "Burglary/Breaking & Entering;Simple Assault","Aggravated Assault;Destruction/Damage/Vandalism of Property;Intimidation",\
+                            "Burglary/Breaking & Entering;Intimidation","Aggravated Assault;Not Specified",\
+                            "Intimidation;Weapon Law Violations","Kidnapping/Abduction;Rape","Pornography/Obscene Material;Simple Assault",\
+                            "Statutory Rape","All Other Larceny;Simple Assault","Aggravated Assault;Pocket-picking",\
+                            "Aggravated Assault;Drug/Narcotic Violations","Aggravated Assault;All Other Larceny","Aggravated Assault;Arson",\
+                            "Arson;Destruction/Damage/Vandalism of Property;Intimidation","Robbery;Sexual Assault With An Object",\
+                            "Shoplifting;Simple Assault","Destruction/Damage/Vandalism of Property;Robbery",\
+                            "Aggravated Assault;Weapon Law Violations","Aggravated Assault;Burglary/Breaking & Entering",\
+                            "Aggravated Assault;Intimidation;Simple Assault","Aggravated Assault;Robbery",\
+                            "Destruction/Damage/Vandalism of Property;Intimidation;Robbery;Simple Assault",\
+                            "Intimidation;Not Specified","Arson;Destruction/Damage/Vandalism of Property;Not Specified" ]
+            
+            if offense_type in violent_lst:
+                return 'Violent'
+            elif offense_type == "Not Specified":
+                return 'Other'
+            else:
+                return "Non-Violent"
+        
+
+    def remove_columns(self, columns):
+        self.data.drop(columns=columns, inplace=True)
+        print("Columns successfully removed.")
+
+    def process_data(self, columns_to_remove):
+        # Remove specified columns
+        self.remove_columns(columns_to_remove)
+
+        #create severity column
+        self.data['SEVERITY'] = self.data['OFFENSE_NAME'].apply(self.map_to_severity)
+
+if __name__ == "__main__":
+    data_path = "C:/Users/chris/Documents/Galvanize/daimil10/Final_Project/Hate_Crime/data/Hate_crime1/combined_data.csv"
+    output_path = "C:/Users/chris/Documents/Galvanize/daimil10/Final_Project/Hate_Crime/data/Hate_crime1/processed_data.csv"  # Specify your desired output path
+    columns_to_remove = [
+        "ORI", "PUB_AGENCY_UNIT", "AGENCY_TYPE_NAME", "DIVISION_NAME", 
+        "REGION_NAME", "POPULATION_GROUP_CODE", "ADULT_VICTIM_COUNT", "JUVENILE_VICTIM_COUNT", 
+        "ADULT_OFFENDER_COUNT", "JUVENILE_OFFENDER_COUNT", "OFFENDER_ETHNICITY", 
+        "MULTIPLE_OFFENSE", "MULTIPLE_BIAS"
+    ] 
+
+    data_processor = HateCrimeAnalyzer(data_path)
+    data_processor.remove_columns(columns_to_remove)
+    
+
+    print(data_processor.data.head())
+    # Call the sum_offenses_by_year function
+    summed_offenses = data_processor.sum_offenses_by_year()
+    
+    print(summed_offenses)
+    # Call the calculate_and_print_victim_counts function
+    data_processor.calculate_and_print_victim_counts()
+
+    data_processor.data.to_csv(output_path, index=False)
+
--- src/new.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def calculate_and_print_victim_counts(dataset):
-    victim_counts = dataset['VICTIM_COUNT'].value_counts()
-    print(victim_counts)
